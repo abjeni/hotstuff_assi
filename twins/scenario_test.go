b0c0eecbfce5a3ba1245cfd8f0d6fd574296aafd
abje <asbjorn.salhus@gmail.com>
2023-02-01 22:28:49 +0100

    add some fuzz and cleanup

--- twins/scenario_test.go
+++ twins/scenario_test.go
@@ -1,14 +1,13 @@
 package twins
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/relab/hotstuff"
 	_ "github.com/relab/hotstuff/consensus/chainedhotstuff"
 )
 
-func basicScenario(t *testing.T) {
+func basicScenario(t *testing.T, id hotstuff.ID, cmd hotstuff.Command, view hotstuff.View, proposer hotstuff.ID) {
 	allNodesSet := make(NodeSet)
 	for i := 1; i <= 4; i++ {
 		allNodesSet.Add(uint32(i))
@@ -22,33 +21,22 @@ func basicScenario(t *testing.T) {
 
 	result, _ := ExecuteScenario(s, 4, 0, 100, "chainedhotstuff", nil, nil)
 
-	fmt.Print(result.NetworkLog)
-	fmt.Print("\n\n\n\n\n\n\n\n")
-	for i, message := range result.Messages {
-		fmt.Printf("%d ---- %v\n", i, message)
-	}
-
-	//fmt.Printf("%v\n", result.Messages)
-
 	oldMessage := result.Messages[0]
 
 	hash := oldMessage.(hotstuff.ProposeMsg).Block.Parent()
 
 	newMessage := hotstuff.ProposeMsg{
-		ID: 1,
+		ID: id,
 		Block: hotstuff.NewBlock(
 			hash,
-			hotstuff.NewQuorumCert(nil, 1, hash),
-			"",
-			1,
-			1,
+			hotstuff.NewQuorumCert(nil, 0, hash),
+			cmd,
+			view,
+			proposer,
 		),
 	}
 
 	result, err := ExecuteScenario(s, 4, 0, 100, "chainedhotstuff", oldMessage, newMessage)
-
-	fmt.Print("\n\n\n\n\n\n\n\n")
-	fmt.Print(result.NetworkLog)
 
 	if err != nil {
 		t.Fatal(err)
@@ -59,32 +47,17 @@ func basicScenario(t *testing.T) {
 	}
 
 	if result.Commits != 1 {
-		t.Errorf("Expected one commit, got (%d)", result.Commits)
+		t.Errorf("Expected one commit (got %d)", result.Commits)
 	}
 }
 
 func TestBasicScenario(t *testing.T) {
-	basicScenario(t)
+	basicScenario(t, 1, "0", 1, 1)
 }
-
-/*
-func NewBlock(parent Hash, cert QuorumCert, cmd Command, view View, proposer ID) *Block {
-	b := &Block{
-		parent:   parent,
-		cert:     cert,
-		cmd:      cmd,
-		view:     view,
-		proposer: proposer,
-	}
-	// cache the hash immediately because it is too racy to do it in Hash()
-	b.hash = sha256.Sum256(b.ToBytes())
-	return b
-}
-*/
 
 func FuzzBasicScenario(f *testing.F) {
-	f.Add()
-	f.Fuzz(func(t *testing.T) {
-		basicScenario(t)
+	f.Add(uint32(1), string("0"), uint64(1), uint32(1))
+	f.Fuzz(func(t *testing.T, id uint32, cmd string, view uint64, proposer uint32) {
+		basicScenario(t, hotstuff.ID(id), hotstuff.Command(cmd), hotstuff.View(view), hotstuff.ID(proposer))
 	})
 }
